e87cbda75154a33f08c26bb079a16902f18a92fd
Océane Marco <oceane.marco.18@gmail.com>
2022-05-26 18:35:10 +0200

    🔒️ Change the rate limiter to 100 requests per 5 minutes for security.

--- groupomania/backend/app.js
+++ groupomania/backend/app.js
@@ -14,8 +14,8 @@ const postsRoutes = require("./routes/posts");
 const usersRoutes = require("./routes/users");
 
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 3000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	windowMs: 5 * 60 * 1000, // 5 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 5 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
